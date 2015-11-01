ab51023e675a37d441d1d145911690647b760a54
afetsis <afetsis@gmail.com>
2015-11-01 13:25:13 +0200

    remove files

--- RTCTemp/__vm/.RTCTemp.vsarduino.h
+++ RTCTemp/__vm/.RTCTemp.vsarduino.h
@@ -83,6 +83,5 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portOutputRegister(P)
 #define portInputRegister(P)
 #define portModeRegister(P)
-#include <..\RTCTemp\RTCNTP.ino>
 #include <..\RTCTemp\RTCTemp.ino>
 #endif
